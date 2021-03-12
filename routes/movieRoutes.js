480d66a554a0076a823748098ee9034ce8590e0c
MichalCh <michal.chlystek@gmail.com>
2021-03-12 08:24:29 +0100

    add title check

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const movies = require('../DB');
 const router = express.Router();
 
-router.post('/title', movies.sendRequest)
+router.post('/title', movies.sendRequest);
+router.post('/comment', movies.createComment);
+
+router.get('/comments', movies.getAllComments)
+router.get('/comments/:titleID', movies.getCommentByMovieID);
+router.get('/movies', movies.getAllMovies);
+router.get('movies/:ID', movies.getMovieById);
+
 
 module.exports = router;
